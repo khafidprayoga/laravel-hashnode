70067b8e825583561e8b3f9fa59f78732e9114fc
Khafid Prayoga <khafidp@pm.me>
2025-03-01 11:03:47 +0700

    explore blade component livewire
    i have learn about section in blade, including content, and display view on the route.
    
    Also change app name on env, and use php artisan make:view components/footer to generate view boilerplate

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<span class="font-mono">
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) & Livewire
+                    {{ \Composer\InstalledVersions::getPrettyVersion('livewire/livewire') }}
+</span>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome')->name('home');
+Route::view('/about', 'pages.about')->name('about');
