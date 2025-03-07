632d459d40bb8862c127e01cd430725602aa3303
Khafid Prayoga <khafidp@pm.me>
2025-03-07 20:03:20 +0700

    change license to cc by nc

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,9 @@
 <div class="text-center py-5 font-mono text-xs px-5">
     <p>
-        Copyright &copy; {{date(format: "Y")}} Khafid Prayoga, All Right Reserved. This website is using cookies.
+        Copyright &copy; {{date(format: "Y")}} Khafid Prayoga, under license <span><a class="underline " href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC</a></span>.
+    </p>
+    <p>
+        This website is using cookies.
     </p>
 
     <p>Powered by <a class="underline hover:cursor-pointer" href="https://hashnode.com" target="_blank">HashNode</a></p>
